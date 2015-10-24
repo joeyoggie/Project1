9899409befefc5189b2e673df9809edf1dfbf776
Youssef Wagieh <joeyoggie@gmail.com>
2015-10-24 05:53:47 +0200

    Merged Fady's ChatPage*

---
+++ app/src/main/java/com/example/android/project1/ChatPage.java
@@ -0,0 +1,46 @@
+package com.example.android.project1;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ChatPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+        setTitle("Nabila");
+        getSupportActionBar().setIcon(R.drawable.juan);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.schedule_message_button)
+        {
+            DialogFragment newFragment = new PopupMessageDialog();
+            newFragment.show(getSupportFragmentManager(), "timePicker");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
