927a6704c7864613ec1b432d37c713dc86322b14
Youssef Wagieh <joeyoggie@gmail.com>
2016-02-20 15:05:05 +0200

    Started adding databases to the project for data persistence.

---
+++ app/src/main/java/DBMessagesContract.java
@@ -0,0 +1,19 @@
+import android.provider.BaseColumns;
+
+/**
+ * Created by Joey on 2/20/2016.
+ */
+public final class DBMessagesContract {
+
+    public DBMessagesContract(){}
+
+    public static abstract class MessageEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "messages";
+        public static final String COLUMN_NAME_ID = "messageID";
+        public static final String COLUMN_NAME_SENDER = "senderUserName";
+        public static final String COLUMN_NAME_RECEPIENT = "recepientUserName";
+        public static final String COLUMN_NAME_CONTENT = "message";
+        public static final String COLUMN_NAME_TIME = "timestamp";
+    }
+}
